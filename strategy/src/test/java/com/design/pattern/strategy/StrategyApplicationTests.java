a09e39682a61b7a2586568465faa0efbd4482617
pedroPolonea <phpolonea@gmail.com>
2019-01-30 22:35:28 -0200

    Includ stratey

---
+++ strategy/src/test/java/com/design/pattern/strategy/StrategyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.design.pattern.strategy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StrategyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
